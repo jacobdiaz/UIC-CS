73d5df434fc228ee34d3b6e549d01d44457c111b
Jacob Diaz <hello@jacobdiaz.dev>
2020-10-09 12:36:35 -0500

    Created Media, Src Folders. Initialized arduinosecuritysystem.ino

--- CS341/Homework3/Homework3/Program.fs
+++ CS341/Homework3/Homework3/Program.fs
@@ -4,46 +4,17 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    
-    printf "filename> "
-    let filename   = System.Console.ReadLine()
-    let data_array = System.IO.File.ReadAllLines(filename)
-    let data_list  = Array.toList data_array 
-    //
-    // convert strings to integers:
-    //        
-    let values = List.map (fun s -> int s) data_list
 
-    printfn "%A" values
-    // 1 :: 2 , 3 , 4 , 5 -> 1
-    // 1,2 :: 3 , 4 , 5 -> 2
-    // 1,2,3 :: 4 , 5 -> 3
-    // 1,2,3,4:: 5 -> 4
-    // 1,2,3,4,5:: []->5
-    // BaseCase reached! -> 0     
-    let rec length valueList = 
-        match valueList with
-        | [] -> 0 
-        | _ :: tail -> 1 + length tail
-         
-    let len = length values
-    printfn "%A" len
-    // 1 :: 2 , 3 , 4 , 5 -> 1
-    // 1,2 :: 3 , 4 , 5 -> 3
-    // 1,2,3 :: 4 , 5 -> 6
-    // 1,2,3,4:: 5 -> 10
-    // 1,2,3,4,5:: []-> 15
-    // BaseCase reached! -> 0 
-    let rec sum valueList =
-        match valueList with
-        | [] -> 0
-        | head :: tail -> head + sum tail
-    let total = sum values
-    printfn "%A" total
-             
-    let sumTerms =  sum values
-    let numTerms =  length values
-    let avg = (float sumTerms) / (float numTerms)
-    printfn "%A" avg
-        
-    0
+// List from 1 - 6
+let list1 = [ 1..6 ] 
+
+// Take the list and split it into two lists by odd and even indexs
+let splitList list = List.foldBack (fun x (l,r) -> x::r, l) list ([],[]) // 
+
+// Take that list and add the corresponding indexes
+let addTwoLists L1 L2 =
+    match L1 with
+    | [] -> [] // add until u two empty lists
+    | h::[] -> L1
+    | h::t -> 
+0
