163d85a9cc1c3c1d7524e884c3f356f286cb7cf4
Jacob Diaz <hello@jacobdiaz.dev>
2020-10-30 13:04:01 -0500

    Commit

--- CS342/Canvas/SampleMultiGUI/src/main/java/Client.java
+++ CS342/Canvas/SampleMultiGUI/src/main/java/Client.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import javafx.application.Platform;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -7,37 +9,34 @@ import java.io.Serializable;
 import java.net.Socket;
 import java.util.function.Consumer;
 
-
-
 public class Client extends Thread{
-
-	
 	Socket socketClient;
 	
 	ObjectOutputStream out;
 	ObjectInputStream in;
-	
+
+	// Consumer is a function interface
 	private Consumer<Serializable> callback;
-	
+
+	// Client constructor accepts a Lambda
 	Client(Consumer<Serializable> call){
-	
 		callback = call;
 	}
-	
+	// This is the code that runs when we do consumer.accept
+	// Platform.runLater(()->{listItems2.getItems().add(data.toString());
 	public void run() {
-		
-		try {
+		try { // try to connect to server program and create streams
 		socketClient= new Socket("127.0.0.1",5555);
 	    out = new ObjectOutputStream(socketClient.getOutputStream());
 	    in = new ObjectInputStream(socketClient.getInputStream());
 	    socketClient.setTcpNoDelay(true);
 		}
 		catch(Exception e) {}
-		
 		while(true) {
-			 
 			try {
+				// Reading in objects from the server
 			String message = in.readObject().toString();
+
 			callback.accept(message);
 			}
 			catch(Exception e) {}
@@ -46,7 +45,6 @@ public class Client extends Thread{
     }
 	
 	public void send(String data) {
-		
 		try {
 			out.writeObject(data);
 		} catch (IOException e) {
--- CS342/Canvas/SampleMultiGUI/src/main/java/GuiServer.java
+++ CS342/Canvas/SampleMultiGUI/src/main/java/GuiServer.java
@@ -37,40 +37,45 @@ public class GuiServer extends Application{
 	public static void main(String[] args) {
 		launch(args);
 	}
-
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
 		primaryStage.setTitle("The Networked Client/Server GUI Example");
-		
+
+		// Creating a s erver button
 		this.serverChoice = new Button("Server");
 		this.serverChoice.setStyle("-fx-pref-width: 300px");
 		this.serverChoice.setStyle("-fx-pref-height: 300px");
-		
-		this.serverChoice.setOnAction(e->{ primaryStage.setScene(sceneMap.get("server"));
-											primaryStage.setTitle("This is the Server");
-				serverConnection = new Server(data -> {
-					Platform.runLater(()->{
+
+		// Server Button Functionality
+		this.serverChoice.setOnAction(e->{
+			// If server btn is clicked get the server scene and set it as the current scene
+			primaryStage.setScene(sceneMap.get("server"));
+			primaryStage.setTitle("This is the Server");
+			// Create a new server
+			serverConnection = new Server(data -> {
+					Platform.runLater(()->{ // Platform run later allows the server thread to pass back a runnable to the application thread job queue
 						listItems.getItems().add(data.toString());
 					});
-
 				});
-											
 		});
-		
-		
+
+		// Creating a client button
 		this.clientChoice = new Button("Client");
 		this.clientChoice.setStyle("-fx-pref-width: 300px");
 		this.clientChoice.setStyle("-fx-pref-height: 300px");
-		
-		this.clientChoice.setOnAction(e-> {primaryStage.setScene(sceneMap.get("client"));
-											primaryStage.setTitle("This is a client");
-											clientConnection = new Client(data->{
-							Platform.runLater(()->{listItems2.getItems().add(data.toString());
-											});
-							});
-							
-											clientConnection.start();
+
+		// If the client button is clicked
+		this.clientChoice.setOnAction(e-> {
+			// Get the scene client and set as currrent scene
+			primaryStage.setScene(sceneMap.get("client"));
+			primaryStage.setTitle("This is a client");
+
+			// Playform.run later will allow the code to run in the application thread instead of the thread in which it was created.
+			clientConnection = new Client(data->{ // Creating a new instance of client
+				Platform.runLater(()->{listItems2.getItems().add(data.toString());
+				});
+			});
+			clientConnection.start(); // Starting a new client thread to the server
 		});
 		
 		this.buttonBox = new HBox(400, serverChoice, clientChoice);
@@ -99,33 +104,23 @@ public class GuiServer extends Application{
                 System.exit(0);
             }
         });
-		
-		 
-		
+
 		primaryStage.setScene(startScene);
 		primaryStage.show();
 		
 	}
 	
 	public Scene createServerGui() {
-		
 		BorderPane pane = new BorderPane();
 		pane.setPadding(new Insets(70));
 		pane.setStyle("-fx-background-color: coral");
-		
 		pane.setCenter(listItems);
-	
 		return new Scene(pane, 500, 400);
-		
-		
 	}
 	
 	public Scene createClientGui() {
-		
 		clientBox = new VBox(10, c1,b1,listItems2);
 		clientBox.setStyle("-fx-background-color: blue");
 		return new Scene(clientBox, 400, 300);
-		
 	}
-
 }
